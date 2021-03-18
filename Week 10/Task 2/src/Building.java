8d8f46c77a7a8c437e6aaadc90a9510681f410c9
bentsen <cph-mb781@cphbusiness.dk>
2021-03-18 13:22:30 +0100

    Done

---
+++ Week 10/Task 2/src/Building.java
@@ -0,0 +1,9 @@
+public class Building
+{
+    private String rooms;
+    private int numberOfBathrooms;
+    private int numberOfFloors;
+    private boolean isOfficeBuilding;
+
+    Building()
+}
---
+++ Week 10/Task 2/src/Room.java
@@ -0,0 +1,32 @@
+public class Room
+{
+    private String walls;
+    private int numberOfDoors;
+    private int numberOfLamps;
+    private int numberOfWindows;
+
+    Room(String walls, int numberOfDoors, int numberOfLamps, int numberOfWindows)
+    {
+
+    }
+
+    public String getWalls()
+    {
+        return walls;
+    }
+
+    public int getNumberOfDoors()
+    {
+        return numberOfDoors;
+    }
+
+    public int getNumberOfLamps()
+    {
+        return numberOfLamps;
+    }
+
+    public int getNumberOfWindows()
+    {
+        return numberOfWindows;
+    }
+}
